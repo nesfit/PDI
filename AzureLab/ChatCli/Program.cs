09c6babd0b183a6bb2ee091d2469340f2dc6330c
Jan Pluskal <ipluskal@fit.vutbr.cz>
2017-10-17 19:51:52 +0200

    Add PDI 2017 lab

---
+++ AzureLab/ChatCli/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ServiceModel;
+using ChatService.Interfaces;
+using ChatService.Models;
+
+namespace ChatCli
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var binding = new BasicHttpBinding();
+            var endpoint =
+                new EndpointAddress(
+                    new Uri(string.Format("http://localhost:26007/ChatApi.svc", Environment.MachineName)));
+            var channelFactory = new ChannelFactory<IChatService>(binding, endpoint);
+            var serviceClient = channelFactory.CreateChannel();
+
+            serviceClient.SendMessage(new ChatMessage {Message = "Message", Sender = "Sender"});
+
+            var messages = serviceClient.GetAllMessages();
+
+            foreach (var chatMessage in messages)
+                Console.WriteLine($"{chatMessage.TimeStamp}, {chatMessage.Sender}> {chatMessage.Message}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AzureLab/ChatService/Models/ChatMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ChatService.Models
+{
+    [DataContract]
+    public class ChatMessage
+    {
+        [DataMember]
+        public DateTime TimeStamp { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public string Sender { get; set; }
+    }
+}
